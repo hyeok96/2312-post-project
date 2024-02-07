6b192997a2e5d5335fadef62887cd9447d0ed7a6
yoonipark <yoonipark09@gmail.com>
2024-02-07 18:22:24 +0900

    test-yj01

--- src/main/java/com/example/PostProject/Application.java
+++ src/main/java/com/example/PostProject/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//test_yj01
+//test-1st
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
